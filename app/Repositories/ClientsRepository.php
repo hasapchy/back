f34af5ae156ad3fe25926b28930063e6e6525456
PavelShevchenko1 <pavelshevchen.ko107@yandex.ru>
2025-02-21 14:21:59 +0500

    модуль клиентов для vue

---
+++ app/Repositories/ClientsRepository.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Client;
+use Illuminate\Support\Facades\DB;
+
+class ClientsRepository {
+    
+
+    function getImemsPaginated($perPage = 20) {
+        
+        $clients = DB::table('clients')
+            ->select('id', 'client_type', 'is_supplier', 'is_conflict', 'first_name', 'last_name', 'contact_person', 'address', 'note', 'status', 'order', 'created_at', 'updated_at')
+            ->paginate($perPage);
+
+        $clientIds = $clients->pluck('id');
+
+        $emails = DB::table('clients_emails')
+            ->whereIn('client_id', $clientIds)
+            ->select('id', 'client_id', 'email')
+            ->get()
+            ->groupBy('client_id');
+
+        $phones = DB::table('clients_phones')
+            ->whereIn('client_id', $clientIds)
+            ->select('id', 'client_id', 'phone')
+            ->get()
+            ->groupBy('client_id');
+
+        foreach ($clients as $client) {
+            $client->emails = $emails->get($client->id, collect());
+            $client->phones = $phones->get($client->id, collect());
+        }
+
+        return $clients;
+    }
+
+    public function create(array $data)
+    {
+        $client = DB::transaction(function () use ($data) {
+            $client = Client::create([
+                'first_name'     => $data['first_name'],
+                'is_conflict'    => $data['is_conflict'] ?? false,
+                'is_supplier'    => $data['is_supplier'] ?? false,
+                'last_name'      => $data['last_name'] ?? "",
+                'contact_person' => $data['contact_person'] ?? null,
+                'client_type'    => $data['client_type'],
+                'address'        => $data['address'] ?? null,
+                'note'           => $data['note'] ?? null,
+                'status'         => $data['status'] ?? true,
+                'discount_value' => $data['discount_value'] ?? 0,
+                'discount_type'  => $data['discount_type'] ?? null,
+            ]);
+
+            if (!empty($data['phones'])) {
+                foreach ($data['phones'] as $phone) {
+                    DB::table('clients_phones')->insert([
+                        'client_id' => $client->id,
+                        'phone'     => $phone,
+                    ]);
+                }
+            }
+
+            if (!empty($data['emails'])) {
+                foreach ($data['emails'] as $email) {
+                    DB::table('clients_emails')->insert([
+                        'client_id' => $client->id,
+                        'email'     => $email,
+                    ]);
+                }
+            }
+
+            return $client;
+        });
+
+        return $client;
+    }
+
+    public function update($id, array $data)
+    {
+        $client = DB::transaction(function () use ($id, $data) {
+            $client = Client::findOrFail($id);
+            $client->update([
+                'first_name'     => $data['first_name'],
+                'is_conflict'    => $data['is_conflict'] ?? false,
+                'is_supplier'    => $data['is_supplier'] ?? false,
+                'last_name'      => $data['last_name'] ?? "",
+                'contact_person' => $data['contact_person'] ?? null,
+                'client_type'    => $data['client_type'],
+                'address'        => $data['address'] ?? null,
+                'note'           => $data['note'] ?? null,
+                'status'         => $data['status'] ?? true,
+                'discount_value' => $data['discount_value'] ?? 0,
+                'discount_type'  => $data['discount_type'] ?? null,
+            ]);
+
+            $existingPhones = DB::table('clients_phones')->where('client_id', $client->id)->pluck('phone')->toArray();
+            $newPhones = $data['phones'] ?? [];
+
+            $phonesToAdd = array_diff($newPhones, $existingPhones);
+            $phonesToRemove = array_diff($existingPhones, $newPhones);
+
+            if (!empty($phonesToAdd)) {
+                foreach ($phonesToAdd as $phone) {
+                    DB::table('clients_phones')->insert([
+                        'client_id' => $client->id,
+                        'phone'     => $phone,
+                    ]);
+                }
+            }
+
+            if (!empty($phonesToRemove)) {
+                DB::table('clients_phones')->where('client_id', $client->id)->whereIn('phone', $phonesToRemove)->delete();
+            }
+
+            $existingEmails = DB::table('clients_emails')->where('client_id', $client->id)->pluck('email')->toArray();
+            $newEmails = $data['emails'] ?? [];
+
+            $emailsToAdd = array_diff($newEmails, $existingEmails);
+            $emailsToRemove = array_diff($existingEmails, $newEmails);
+
+            if (!empty($emailsToAdd)) {
+                foreach ($emailsToAdd as $email) {
+                    DB::table('clients_emails')->insert([
+                        'client_id' => $client->id,
+                        'email'     => $email,
+                    ]);
+                }
+            }
+
+            if (!empty($emailsToRemove)) {
+                DB::table('clients_emails')->where('client_id', $client->id)->whereIn('email', $emailsToRemove)->delete();
+            }
+
+            return $client;
+        });
+
+        return $client;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AppController;
 use App\Http\Controllers\Api\CategoriesController;
+use App\Http\Controllers\api\ClientController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\UsersController;
@@ -55,4 +56,9 @@ Route::middleware('auth:api')->group(function () {
     Route::get('services', [ProductController::class, 'services']);
     Route::post('products', [ProductController::class, 'store']);
     Route::post('products/{id}', [ProductController::class, 'update']);
+
+    //clients
+    Route::get('clients', [ClientController::class, 'getClients']);
+    Route::post('clients', [ClientController::class, 'createClient']);
+    Route::put('clients/{id}', [ClientController::class, 'updateClient']);
 });
