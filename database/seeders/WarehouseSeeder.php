789ac37ea29e66b257f4cb0aca8003f396293cfb
Mekan <mekan.developer@gmail.com>
2026-03-31 16:25:16 +0500

    fix: WarehouseSeeder sprawil

--- database/seeders/WarehouseSeeder.php
+++ database/seeders/WarehouseSeeder.php
@@ -3,17 +3,29 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Company;
 use App\Models\Warehouse;
 use App\Models\WhUser;
 
 class WarehouseSeeder extends Seeder
 {
+    /**
+     * Создает основной склад и привязывает его к компании.
+     */
     public function run()
     {
+        // Берем первую существующую компанию без создания новой записи.
+        $company = Company::query()->first();
+
+        if (!$company) {
+            return;
+        }
+
         $warehouse = Warehouse::updateOrCreate([
             'id' => 1
         ], [
             'name' => 'Основной склад',
+            'company_id' => $company->id,
         ]);
 
         // Добавляем пользователя с ID 1 в основной склад
